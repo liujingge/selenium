c92169ebe6da19933d5c0b23cf92b2bd20d10670
liujingge <69623888+liujingge@users.noreply.github.com>
2020-08-17 18:00:33 +0800

    自动化测试验证码

--- read_image.py
+++ read_image.py
@@ -1,6 +1,15 @@
 #coding=utf-8
 import pytesseract
 from PIL import Image
-image = Image.open('F:\\learn\\selenium\\one.png')
-text = pytesseract.image_to_string(image)
-print(text)
+from ShowapiRequest import ShowapiRequest
+# image = Image.open('F:\\learn\\selenium\\imooc1.png')
+# text = pytesseract.image_to_string(image)
+# print(text)
+
+r = ShowapiRequest("http://route.showapi.com/1274-2","my_appId","my_appSecret" )
+r.addBodyPara("imgUrl", "")
+r.addBodyPara("base64", "")
+r.addFilePara("imgFile", r"F:\\learn\\selenium\\imooc1.png")#文件上传设置
+res = r.post()
+text = res.json()['showapi_res_body']['Result']
+print(text) # 返回信息
--- start_browser.py
+++ start_browser.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import time
 import random
 from PIL import Image
+from ShowapiRequest import ShowapiRequest
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 # driver = webdriver.Edge()
@@ -10,14 +11,33 @@ driver.get("http://www.5itest.cn/register")
 time.sleep(5)
 print(EC.title_contains("注册"))
 email_element = driver.find_element_by_id("register_email")
-driver.save_screenshot("C:/imooc.png")
+driver.save_screenshot("F:\\learn\\selenium\\imooc1.png")
 code_element = driver.find_element_by_id("getcode_num")
 print(code_element.location)
 left = code_element.location['x']
 top = code_element.location['y']
 right = code_element.size['width']+left
 height = code_element.size['height']+top
-im = Image.open("C:/imooc.png")
+im = Image.open("F:\\learn\\selenium\\imooc1.png")
 img = im.crop((left,top,right,height))
-img.save("C:/imooc1.png")
+img.save("F:\\learn\\selenium\\imooc1.png")
+
+r = ShowapiRequest("http://route.showapi.com/1274-2","my_appId","my_appSecret" )
+r.addBodyPara("imgUrl", "")
+r.addBodyPara("base64", "")
+r.addFilePara("imgFile", r"F:\\learn\\selenium\\imooc1.png")#文件上传设置
+res = r.post()
+text = res.json()['showapi_res_body']['Result']
+print(text) # 返回信息
+time.sleep(5)
+driver.find_element_by_id("captcha_code").send_keys(text)
+
+
+
+driver.find_element_by_class_name("controls")
+locals = (By.CLASS_NAME,"controls")
+WebDriverWait(driver,1).until(EC.visibility_of_element_located(locator))
+
+print(email_element.get_attribute("placeholder"))
+email_element.send_keys("123@qq.com")
 
